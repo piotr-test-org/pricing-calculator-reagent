69ecce7b082e2d42580c8b32a08ec24e4dbfcc83
Piotr Roterski <pt.roterski@gmail.com>
2019-10-22 22:00:57 +0200

    Add fetch-data

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,5 +1,20 @@
 (ns app.main
-  (:require [reagent.core :as r]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as r]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+
+(defonce app-state (r/atom {}))
+
+(def urls
+  {:licenses "https://sos-de-muc-1.exo.io/exercises-pricing-data/licenses.json"
+   :opencompute "https://sos-de-muc-1.exo.io/exercises-pricing-data/opencompute.json"
+   :sos "https://sos-de-muc-1.exo.io/exercises-pricing-data/sos.json"})
+
+(defn fetch-data [source]
+  (let [url (source urls)]
+    (go (let [response (<! (http/get url {:with-credentials? false}))]
+          (swap! app-state assoc-in [:data source] (:body response))))))
 
 (def app
   [:div
@@ -9,3 +24,6 @@
   (println "[main]: hello world!")
   (r/render-component app
                       (.getElementById js/document "app")))
+
+(comment
+  (fetch-data :sos))
