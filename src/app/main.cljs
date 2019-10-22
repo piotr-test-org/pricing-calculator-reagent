5a6c02c6c6a3bac4b45e04aa0b857639e8a2da4b
Piotr Roterski <pt.roterski@gmail.com>
2019-10-22 22:35:18 +0200

    Enable hot code reloading

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -4,12 +4,14 @@
     [app.views.app :refer [app]]
     [reagent.core :as r]))
 
+(defn ^:dev/after-load start []
+  (r/render-component app
+                      (.getElementById js/document "app")))
 
 (defn main! []
   (println "[main]: Initializing app")
   (data/fetch-all)
-  (r/render-component app
-                      (.getElementById js/document "app")))
+  (start))
 
 (comment
   (fetch-data :sos))
