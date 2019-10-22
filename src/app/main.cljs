394d831d2c2cafb161b41dfdefd5aaa02db21e93
Piotr Roterski <pt.roterski@gmail.com>
2019-10-22 21:26:13 +0200

    Add reagent

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,4 +1,11 @@
-(ns app.main)
+(ns app.main
+  (:require [reagent.core :as r]))
+
+(def app
+  [:div
+   [:h1 "Price Calculator"]])
 
 (defn main! []
-  (println "[main]: hello world!"))
+  (println "[main]: hello world!")
+  (r/render-component app
+                      (.getElementById js/document "app")))
