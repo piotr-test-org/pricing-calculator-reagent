71d103cb0aa89580dab495cd36590d7f2f11ad99
Piotr Roterski <pt.roterski@gmail.com>
2019-10-23 21:57:10 +0200

    Add adding and removing products

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -2,18 +2,18 @@
   (:require [reagent.core :as r]))
 
 (defonce app-state (r/atom {:currency :eur
-                            :products [{:id 1
-                                        :instance :running_storage_huge
-                                        :license :license_win
-                                        :volume_data 100}
-                                       {:id 2
-                                        :instance :running_gpu_medium
-                                        :license nil
-                                        :volume_data nil}
-                                       {:id 3
-                                        :instance :running_medium
-                                        :license nil
-                                        :volume_data 200}]}))
+                            :products {1 {:id 1
+                                          :instance :running_storage_huge
+                                          :license :license_win
+                                          :volume_data 100}
+                                       2 {:id 2
+                                          :instance :running_gpu_medium
+                                          :license nil
+                                          :volume_data nil}
+                                       3 {:id 3
+                                          :instance :running_medium
+                                          :license nil
+                                          :volume_data 200}}}))
 
 (defn set-currency [currency]
   (swap! app-state assoc :currency (keyword currency)))
@@ -22,7 +22,19 @@
   (:currency @app-state))
 
 (defn products []
-  (:products @app-state))
+  (-> @app-state
+      :products
+      vals))
 
 (defn data [source]
   (get-in @app-state [:data source (currency)]))
+
+(defn add-product []
+  (let [id (random-uuid)]
+    (swap! app-state assoc-in [:products id] {:id id
+                                              :instance nil
+                                              :license nil
+                                              :volume_data nil})))
+
+(defn remove-product [id]
+  (swap! app-state update-in [:products] dissoc id))
--- src/app/views/product_list.cljs
+++ src/app/views/product_list.cljs
@@ -1,6 +1,6 @@
 (ns app.views.product-list
-  (:require [app.state :refer [app-state set-currency data]]
-            ["@smooth-ui/core-sc" :refer [Box Boxer Input Select Option]]))
+  (:require [app.state :refer [data add-product products remove-product]]
+            ["@smooth-ui/core-sc" :refer [Box Boxer Input Select Option Button Separator]]))
 
 (defn product-item [{:keys [id instance license volume_data]}]
   [:> Box {:key id
@@ -13,12 +13,14 @@
    [:label "License"]
    [:> Input {:value license :disabled true :m 1}]
    [:label "Volume"]
-   [:> Input {:type :number :value volume_data :disabled true :m 1}]])
+   [:> Input {:type :number :value volume_data :disabled true :m 1}]
+   [:> Button {:variant :danger :onClick #(remove-product id) :align-self :flex-end} "Remove"]])
 
 (defn product-list []
   [:> Box
    [:h3 "Products"]
    (-> [:> Box]
-       (concat (map product-item (:products @app-state)))
-       vec)])
+       (concat (map product-item (products)))
+       vec)
+   [:> Button {:variant :primary :onClick #(add-product)} "Add"]])
 
