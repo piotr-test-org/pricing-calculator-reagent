3f5748ed752a716e1ef864c2fdc658d8abfd0460
Piotr Roterski <pt.roterski@gmail.com>
2019-10-23 22:58:14 +0200

    Extract product-item to separate file

--- src/app/views/product_list.cljs
+++ src/app/views/product_list.cljs
@@ -1,38 +1,7 @@
 (ns app.views.product-list
-  (:require [app.state :refer [data add-product data products instances licenses remove-product update-product]]
-            ["@smooth-ui/core-sc" :refer [Box Boxer Input Select Option Button Separator]]))
-
-(defn product-item [{:keys [id instance license volume_data]}]
-  [:> Box {:key id
-           :p 1
-           :m 3
-           :border 1.5
-           :border-radius 5}
-   [:label "Instance"]
-   (-> [:> Select {:value instance
-                   :on-change #(update-product id :instance (.. % -target -value))
-                   :m 1}]
-       (concat (map (fn [[label value]] [:option {:value value} label]) (instances)))
-       vec)
-
-   [:label "License"]
-   (-> [:> Select {:value license
-                   :on-change #(update-product id :license (.. % -target -value))
-                   :m 1}]
-       (concat (map (fn [[label value]] [:option label]) (licenses)))
-       vec)
-   [:label "Volume"]
-   [:> Input {:type :number
-              :min 0
-              :max 10000
-              :value volume_data
-              :on-change #(update-product id :volume_data (.. % -target -value))
-              :m 1}]
-   [:label "GB"]
-   [:> Button {:onClick #(remove-product id)
-               :variant :danger
-               :ml 5
-               :align-self :flex-end} "Remove"]])
+  (:require [app.views.product-item :refer [product-item]]
+            [app.state :refer [data add-product products]]
+            ["@smooth-ui/core-sc" :refer [Box Button]]))
 
 (defn product-list []
   [:> Box
