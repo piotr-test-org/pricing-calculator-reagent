88d8ac55bc5fa1341f06b4a9ccd279dcfe712ed2
Piotr Roterski <pt.roterski@gmail.com>
2019-10-26 18:09:44 +0200

    Set initial state to empty product list

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -2,17 +2,6 @@
   (:require [reagent.core :as r]))
 
 (defonce app-state (r/atom {:currency :eur
-                            :products {1 {:id 1
-                                          :instance :running_storage_huge
-                                          :license :license_win
-                                          :volume_data 100}
-                                       2 {:id 2
-                                          :instance :running_gpu_medium
-                                          :license :none
-                                          :volume_data 0}
-                                       3 {:id 3
-                                          :instance :running_medium
-                                          :license :none
-                                          :volume_data 200}}}))
+                            :products {}}))
 
 
